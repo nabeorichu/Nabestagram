b63b520a2c2cdb4214f7e6111f841b26ada33dc9
優也渡辺 <yuuyawatanabe@yuuyawatanabe-no-MacBook-Air.local>
2016-10-21 19:13:46 +0900

    メール機能追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,8 +16,10 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(pictures_params)
+    @picture.user_id = current_user.id
     if @picture.save
       redirect_to pictures_path, notice: "投稿が完了しました！"
+      NoticeMailer.sendmail_picture(@picture).deliver
     else
       render action: 'new'
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,24 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
 
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
@@ -76,4 +59,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://ancient-plateau-93530.herokuapp.com/'}
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentiication: plain,
+    enable_startttls_auto: true
+  }
 end
